0fd09f028e979770cf9c87e63ccf2c8719138dbe
TubTub62 <TubTub62@users.noreply.github.com>
2019-03-22 21:48:09 +0100

    test

--- js/list.js
+++ js/list.js
@@ -1,18 +1,5 @@
 function ListInsert () {
-document.write(<ul class="nav">
-<li> <a href="https://folder6.github.io/">Home</a> </li>
-<li> <a href="https://folder6.github.io/html/ralf.html">Ralf</a> </li>
-<li> <a href="https://folder6.github.io/html/mads.html">Mads</a> </li>
-<li> <a href="https://folder6.github.io/html/felix.html">Felix</a> </li>
-<li> <a href="https://folder6.github.io/html/frederik.html">Frederik</a> </li>
-<li> <a href="https://folder6.github.io/html/lucas.html">Lucas</a> </li>
-<li> <a href="https://folder6.github.io/html/bonk.html">Bonk</a> </li>
-<li> <a href="https://folder6.github.io/html/tubtub.html">ButBut</a> </li>
-<li> <a href="https://folder6.github.io/html/albert.html">Bært</a> </li>
-<li> <a href="https://folder6.github.io/html/mikki.html">Mikki</a> </li>
-<li> <a href="https://folder6.github.io/html/download.html">Download</a> </li>
-<li> <a href="https://folder6.github.io/html/peter.html"><strike>Peter</strike></a> </li>
-<li> <a href="https://folder6.github.io/html/links.html">Links</a> </li>
-<li> <a href="https://folder6.github.io/html/chat.html">Chat</a> </li>
-</ul>)
+document.open()
+document.write("<p1>small code test</p1>")
+document.close
 }
